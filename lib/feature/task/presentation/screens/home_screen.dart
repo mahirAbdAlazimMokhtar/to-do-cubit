a6d08ff5ceec0dc4d5353fe55d46abd0f7561d79
Mahir <mahr.bwb@gmail.com>
2023-11-14 18:36:34 +0300

    add FAB Button to the home Screen

--- lib/feature/task/presentation/screens/home_screen.dart
+++ lib/feature/task/presentation/screens/home_screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/core/utils/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.primary,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
